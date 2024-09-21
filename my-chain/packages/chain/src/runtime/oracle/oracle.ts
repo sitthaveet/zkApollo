4c3e484ac8ee40d3e0d52c9b8c7a7efce4f9fcaa
Son <sitthavee.t@gmail.com>
2024-09-21 10:56:12 +0800

    update oracle.ts

--- my-chain/packages/chain/src/runtime/oracle/oracle.ts
+++ my-chain/packages/chain/src/runtime/oracle/oracle.ts
@@ -6,23 +6,22 @@ import {
   state,
 } from "@proto-kit/module";
 import { UInt224, UInt64 } from "@proto-kit/library";
-import { PublicKey, Field, Bool, Provable } from "o1js";
-
-@runtimeModule()
+import { PublicKey, Field, Bool, Provable, Signature } from "o1js";
 
 // public key of Copper
 let _copperPublicKey = PublicKey.fromBase58("B62qoAE4rBRuTgC42vqvEyUqCGhaZsW58SKVW4Ht8aYqP9UTvxFWBgy");
 
-
-export class OracleModule extends RuntimeModule {
+@runtimeModule()
+export class OracleModule extends RuntimeModule<Record<string, never>> {
 
   // setup state variables
   @state() public realAmount = State.from(UInt224);
   @state() public copperPublicKey = State.from(PublicKey);
+  @state() public lockAmount = State.from(UInt224);
 
   // define event
   events = {
-    verified: Field,
+    verified: UInt224,
   };
 
   // init method
@@ -31,26 +30,79 @@ export class OracleModule extends RuntimeModule {
   ): Promise<void> {
     this.realAmount.set(UInt224.from(0));
     this.copperPublicKey.set(_copperPublicKey);
+    this.lockAmount.set(UInt224.from(0));
   }
 
-  // method oracle verify from Copper API
-  @runtimeMethod() public async verifyAmount(realAmount: UInt224, targetAmount: UInt224) {
-    // Get the oracle public key from the zkApp state
-    const copperPublicKey = this.copperPublicKey.get();
-    assert(this.copperPublicKey.get().equals(copperPublicKey), "Copper public key does not match");
 
+  public async getRealAmount(): Promise<UInt224> {
+    const realAmount = await this.realAmount.get();
+    return UInt224.from(realAmount.value.toString());
+  }
 
-    // Evaluate whether the signature is valid for the provided data
-    const validSignature = signature.verify(copperPublicKey, []); // to be implemented
-    validSignature.assertTrue();
+  public async getLockAmount(): Promise<UInt224> {
+    const lockAmount = await this.lockAmount.get();
+    return UInt224.from(lockAmount.value.toString());
+  }
 
-    // Check that the provided credit score is 700 or higher
-    realAmount.assertGreaterThanOrEqual(UInt224.from(targetAmount));
+  public async verifyIfRealAmountIsMoreThanTarget(targetAmount: UInt224): Promise<Bool> {
+    const realAmount = await this.realAmount.get();
+    const isRealAmountMoreThanTarget = targetAmount.lessThanOrEqual(UInt224.from(realAmount.value.toString()));
+    return isRealAmountMoreThanTarget;
+  }
+
+  private async penaltyOwner(): Promise<UInt224> {
+    // TODO: implement penalty for Alice
+    return UInt224.from(0);
+  }
+
+  // method oracle verify response from Copper API for checking from their client
+  @runtimeMethod() 
+  public async verifyReserve(realAmount: UInt224, signature: Signature, targetAmount: UInt224) {
+    // // Get the oracle public key from the zkApp state
+    // const copperPublicKey = this.copperPublicKey.get();
+    
+    // // Evaluate whether the signature is valid for the provided data
+    // const validSignature = signature.verify(copperPublicKey, [realAmount.toString()]);
+    // assert(validSignature, "Invalid signature");
 
     // set realAmount to the state
     this.realAmount.set(realAmount);
 
-    // Emit an event
-    this.emitEvent('verified', realAmount);
+    // Check if real amount is greater or equal to the target amount
+    const isRealAmountMoreThanTarget = await this.verifyIfRealAmountIsMoreThanTarget(targetAmount);
+
+    // penalty action if the reserve is below target
+    const penaltyAction = Provable.if(
+      // check if real amount is more than target amount
+      isRealAmountMoreThanTarget,
+      // // if reserve > target
+      // await this.getLockAmount(),
+
+      // // if reserve < target, penalty Alice and return false
+      // await this.penaltyOwner()
+    );  
+
+    // set lock amount to the state
+    // this.lockAmount.set(UInt224.from(penaltyAction));
   }
+
+
+  // method oracle verify response from Copper API for minting
+  @runtimeMethod() 
+  public async verifyReserveForMinting(realAmount: UInt224, targetAmount: UInt224) {
+    // set realAmount to the state
+    this.realAmount.set(realAmount);
+
+    // Check if real amount is greater or equal to the target amount
+    const isRealAmountMoreThanTarget = await this.verifyIfRealAmountIsMoreThanTarget(targetAmount);
+    assert(isRealAmountMoreThanTarget, "You have not enough reserves to mint the synthetic asset");
+
+    // implement lock MINA on-chain
+
+    // implement minting synthetic asset
+    
+  }
+
+
+
 }
